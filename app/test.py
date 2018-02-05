072c86a147e233067f4b05649513d19e8c89f9aa
warmaxx <34451050+warmaxx@users.noreply.github.com>
2018-02-05 12:28:27 +0300

    get_session

--- app/test.py
+++ app/test.py
@@ -33,11 +33,36 @@ async def test_get(request):
         info = {'status': 'failed', 'message': str(e)}
         return web.Response(text=json.dumps(info), status=500)
 
+async def authorize(app, handler):
+    async def middleware(request):
+        # def check_path(path):
+        #     result = True
+        #     for r in ['/login', '/static/', '/signin', '/signout', '/_debugtoolbar/']:
+        #         if path.startswith(r):
+        #             result = False
+        #     return result
 
-app = web.Application(middlewares=[aiohttp_session.session_middleware(aiohttp_session.SimpleCookieStorage())])
+        session = await get_session(request)
+        if session.get("user"):
+            print('GET Session ')
+            return await handler(request)
+        # elif check_path(request.path):
+        #     url = request.app.router['login'].url()
+        #     raise web.HTTPFound(url)
+        #     return handler(request)
+        else:
+            print('NO Session')
+            return await handler(request)
+
+    return middleware
+
+
+
+app = web.Application(middlewares=[aiohttp_session.session_middleware(aiohttp_session.SimpleCookieStorage()),
+                                   authorize])
 aiohttp_jinja2.setup(app,
-                     loader=jinja2.FileSystemLoader('C:/Users/Алексей/PycharmProjects/testhttp/'))
-# loader=jinja2.FileSystemLoader('C:/Users/MaksyaginAV/PycharmProjects/testhttp'))
+                     # loader=jinja2.FileSystemLoader('C:/Users/Алексей/PycharmProjects/testhttp/'))
+                    loader=jinja2.FileSystemLoader('C:/Users/MaksyaginAV/PycharmProjects/testhttp'))
 
 app.router.add_get('/', test_get)
 app.router.add_get('/auth/', auth.AuthView, name='auth:get')
